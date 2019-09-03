c1deae450c56ae4f6f8e0f4071f91304c5a8f046
Guilherme <guilherme.ferreira28@outlook.com>
2019-09-03 19:38:36 +0200

    Fix nav bar

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+    'use strict';
+
+    var myNav = document.getElementById("nav-bar");
+    var anchorElements = document.getElementsByTagName('a');
+
+    if (document.documentElement.scrollTop >= 200 || document.body.scrollTop >= 200) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-grey");
+
+        for (var i = 0; i < anchorElements.length; i++) {
+            anchorElements[i].classList.add('white-text');
+            anchorElements[i].classList.remove('black-text');
+        }
+    } else {
+        myNav.classList.remove("nav-colored");
+        myNav.classList.add("nav-grey");
+
+        for (var i = 0; i < anchorElements.length; i++) {
+            anchorElements[i].classList.remove('white-text');
+            anchorElements[i].classList.add('black-text');
+        }
+    }
+}
